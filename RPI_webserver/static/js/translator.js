b7895dc8eb9f415ecb7254a08631a160469aecb8
Ming Xuan Choy <iancmx@hotmail.com>
2019-03-27 18:33:17 -0700

    2nd workaround attempt

--- RPI_webserver/static/js/translator.js
+++ RPI_webserver/static/js/translator.js
@@ -2,6 +2,8 @@ var validChars = new Set(["a","b","c","d","e","f","g","h","i","j","k","l","m","n
     "u","v","w","x","y","z","A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T", 
     "U","V","W","X","Y","Z","#","0","1","2","3","4","5","6","7","8","9",";","'","?","!",":","-",","," "]); 
 
+var text = "(Default)"
+
 function printBraille() {
     var input = document.getElementById('myTextArea').value;
     console.log(input);
@@ -38,14 +40,14 @@ function printBraille() {
 window.onload = loadText()
 
 function loadText() {
+    updateText(text)
     $.ajax({
         type:"GET",
         url:"/updateText",
         dataType:"json",
         async:false,
         success: function(data){
-            text = data
-            updateText(text)
+            text = data;
         },
         complete: function(){
             setTimeout(loadText, 1000);
