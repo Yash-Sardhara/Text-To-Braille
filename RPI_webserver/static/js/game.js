7aec2cb5447ebc30557513e3ede241514ecf5439
andixiong <xiongandi@gmail.com>
2019-03-28 19:03:14 -0700

    hey

--- RPI_webserver/static/js/game.js
+++ RPI_webserver/static/js/game.js
@@ -4,40 +4,12 @@ var validAlphabets = new Set(["a","b","c","d","e","f","g","h","i","j","k","l","m
 var validNums = new Set(["0","1","2","3","4","5","6","7","8","9"]);
 var validPuncs = new Set(["#",";","'","?","!",":","-",","," ", "."])
 
-
+var total = validAlphabets.length + validNums.length + validPuncs.length;
+console.log(total);
 function printBraille() {
-    var input = document.getElementById('myTextArea').value;
-    console.log(input);
-    // check if the input is valid
-    var prevIsNum = false;
-    var sum = 0;
-    if (input.length == 0) {
-        document.getElementById('modalTitle').innerHTML = "Submission Failed!";
-        document.getElementById('modalBody').innerHTML = "Empty input";
-        return;
-    } 
-    for (var i=0; i<input.length; i++) {
-        if(!validAlphabets.has(input[i]) && !validNums.has(input[i]) && !validPuncs.has(input[i]) && input[i]!=" "){
-            document.getElementById('modalTitle').innerHTML = "Submission Failed!";
-            document.getElementById('modalBody').innerHTML = "Invalid Input: ' ".concat(input[i], " '", '.');
-            return;
-        }
-        if(validNums.has(input[i]) && isNum == false) {
-            sum++;
-        }
-        if(!validNums.has(input[i]) && isNum == true && input[i]!=" ") {
-            sum++;
-        }
-        sum++;
-    }
-
-    if (sum > 52) {
-        document.getElementById('modalTitle').innerHTML = "Submission Failed!";
-        document.getElementById('modalBody').innerHTML = "Exceeded max characters allowed by ".concat(sum-52, " characters.");
-        return;
-    } 
+    var count = 0;
+    Math.floor((Math.random() * total) + 1);
     
-
     // send request to server
     // $.ajax({
     //     type:"POST",
